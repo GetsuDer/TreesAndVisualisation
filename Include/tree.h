783e74a79c17ccffc41f9084cb87edd2f3d5d36f
Svetlana <svetlo_nika@mail.ru>
2020-11-21 16:55:45 +0300

    Now generating correct input for dot

--- Include/tree.h
+++ Include/tree.h
@@ -6,12 +6,24 @@ private:
     int children_number;
     Node *parent;
     Node **childs;
-    char *node_name;
-public:
-    Node(char *name);
-    ~Node();
+    int operation;
+    double value;
     int visualize(int fd);
+    int visualize_tree_rec(int fd);
+public:
+    Node(int _operation);
+    Node(double _value);
+    ~Node();
+    int export_dot(int fd, char *graph_name = NULL);
     int add_child(Node *child);
 };
 
+enum Node_Types {
+    CONSTANT = 0,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    POWER
+};
 #endif
--- Source/tree.cpp
+++ Source/tree.cpp
@@ -1,29 +1,100 @@
 #include <string>
 #include <cstdio>
+#include <cassert>
 
 #include "tree.h"
 
-Node::Node(char *name) {
+//! \brief Node constructor
+Node::Node(int _operation) {
     children_number = 0;
     childs = NULL;
     parent = NULL;
-    node_name = name;
+    operation = _operation;
 }
 
+Node::Node(double _value) {
+    children_number = 0;
+    childs = NULL;
+    parent = NULL;
+    operation = CONSTANT;
+    value = _value;
+}
+
+//! \brief Node destructor
 Node::~Node() {
     free(childs);
 }
-//! \brief Write description of node and labels of node in graphivz-readable format
+//! \brief Write description of node in graphivz-readable format
 //! \param [in] fd File descriptor
-//! \return Returns 0 in success
+//! \return Returns 0 in success -1 else
 int
 Node::visualize(int fd) {
-    for (int i = 0; i < children_number; i++) {
-        dprintf(fd, "%s->%s\n", node_name, childs[i]->node_name);
+    assert(fd > 0);
+    switch (this->operation) {
+        case CONSTANT: 
+            dprintf(fd, "\"%lf\"", this->value);
+            break;
+        case MUL:
+            dprintf(fd, "\"*\"");
+            break;
+        case DIV:
+            dprintf(fd, "\"/\"");
+            break;
+        case ADD:
+            dprintf(fd, "\"+\"");
+            break;
+        case SUB:
+            dprintf(fd, "\"-\"");
+            break;
+        case POWER:
+            dprintf(fd, "\"^\"");
+            break;
+        default:
+            fprintf(stderr, "Wrong operation: %d\n", this->operation);
+            return -1;
     } 
     return 0;
 }
 
+//! \brief Recursive function for tree visualization generation
+//! \param [in] fd File descriptor
+//! \return Returns 0 in success -1 else
+int
+Node::visualize_tree_rec(int fd) {
+    for (int i = 0; i < this->children_number; i++) {
+        this->visualize(fd);
+        dprintf(fd, "->");
+        this->childs[i]->visualize(fd);
+        dprintf(fd, ";\n");
+        if (this->childs[i]->visualize_tree_rec(fd)) {
+            fprintf(stderr, "Error during recursive visualization\n");
+            return -1;
+        }
+    }
+    return 0;
+}
+//! \brief Writes tree description in dot-readable format
+//! \param [in] root Tree root
+//! \param [in] fd File descriptor
+//! \return Returns 0 in success, -1 else
+int
+Node::export_dot(int fd, char *graph_name) {
+    assert(fd >= 0);
+
+    dprintf(fd, "digraph ");
+    if (graph_name) {
+        dprintf(fd, "%s {\n", graph_name);
+    } else {
+        dprintf(fd, "G {\n");
+    }
+    this->visualize_tree_rec(fd);
+    dprintf(fd, "}\n");
+    return 0;
+}
+
+//! \brief Made link between two nodes by adding one node as child to another
+//! \param [in] child Pointer to new child node
+//! \return Returns 0 in success -1 else
 int
 Node::add_child(Node *child) {
     children_number++;
