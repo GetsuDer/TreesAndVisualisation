b9593ec0f24e08dcdb25204487ab044bb4b9fcf7
Svetlana <svetlo_nika@mail.ru>
2020-12-10 17:10:11 +0300

    Now can parse a + b - c ...

--- Source/rec_desc.cpp
+++ Source/rec_desc.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <ctype.h>
+#include <cmath>
 
 #include "rec_desc.h"
 
@@ -19,20 +20,14 @@ skip_spaces(struct Env *env) {
     return;
 }
 
-static double
-make_double(int first_part, int second_part) {
-    double res = second_part;
-    while (res > 1) {
-        res /= 10;
-    }
-    return res + first_part;
-}
-
 //! \brief Read unsigned integer
 static int 
-GetN(struct Env *env) {
+GetNumber(struct Env *env) {
+    skip_spaces(env);
+
     if (env->current_ind >= env->str_size) {
         env->error = NO_SYMBOL;
+        return 0;
     }
     int val = 0;
     while (env->str[env->current_ind] >= '0' && env->str[env->current_ind] <= '9') {
@@ -40,6 +35,48 @@ GetN(struct Env *env) {
         env->current_ind++;
     }
     return val;
+}
+
+
+static double
+GetDouble(struct Env *env) {
+    skip_spaces(env);
+    if (env->current_ind >= env->str_size) {
+        env->error = NO_SYMBOL;
+        return NAN;
+    }
+    double res = GetNumber(env);
+    
+    if (NEXT('.', env)) {
+        env->current_ind++;
+        int old_ind = env->current_ind;
+        res += GetNumber(env) / pow(10, env->current_ind - old_ind);
+    }
+    return res;
+}
+
+static double 
+GetSum(struct Env *env) {
+    if (env->current_ind >= env->str_size) {
+        env->error = NO_SYMBOL;
+        return NAN;
+    }
+    double res = GetDouble(env);
+    while (true) {
+        skip_spaces(env);
+        switch(env->str[env->current_ind]) {
+            case '+':
+                env->current_ind++;
+                res += GetDouble(env);   
+                break;
+            case '-':
+                env->current_ind++;
+                res -= GetDouble(env);
+                break;
+            default:
+                return res;
+        }
+    }
 }
 
 double
@@ -50,16 +87,7 @@ Parse_All(char *str, int str_length) {
     env.str_size = str_length;
     env.error = OK;
     
-    skip_spaces(&env);
-    int first_part = GetN(&env);
-    int second_part = 0;
-    if (NEXT('.', &env)) {
-        env.current_ind++;
-        second_part = GetN(&env);
-    }
-    double res = make_double(first_part, second_part); 
-    skip_spaces(&env);
-
+    double res = GetSum(&env);
     REQUIRE('$', &env);
     if (env.error) {
         fprintf(stderr, "Error during recursive descent\n");
